e92ade5a66669d8c4237dd5e9442dc8d904b594d
JorgeMontoya <Jamontoyao@uqvirtual.edu.co>
2022-07-18 21:59:25 -0500

    se crea el metodo que permite dar movimiento al circulo

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@
         this.game_over = false;
         this.bars = [];   // barras a la orilla del juego
         this.ball = null;
+        this.playing = false; 
     }
 
     self.Board.prototype = {
@@ -26,11 +27,22 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
         board.ball = this;
         this.kind = "circle";
 
+        
+
 
      }
+     self.Ball.prototype = {
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+
+        }
+    }
+
 })();
 (function(){
     self.Bar = function(x,y,width,height,board){
@@ -77,9 +89,14 @@
             };
         },
         play: function(){
-            this.clean();
 
-            this.draw();
+            if(this.board.playing){
+                this.clean();
+
+                this.draw();
+                this.board.ball.move();
+            }
+             
         }
     }
     function draw(ctx,element){
@@ -110,21 +127,33 @@ var  board_view = new BoardView(canvas,board);
 var ball = new Ball(350,100,10, board);
 
 document.addEventListener("keydown", function(ev){
-    ev.preventDefault();
     console.log(ev.keyCode);
     if(ev.keyCode == 38){
+        ev.preventDefault();
+
         bar.up();
     }
     else if(ev.keyCode == 40 ){
+        ev.preventDefault();
+
         bar.down();
     }//w
     else if(ev.keyCode == 87 ){
+        ev.preventDefault();
+
         bar_2.up();
     }//s
     else if(ev.keyCode == 83 ){
+        ev.preventDefault();
+
         bar_2.down();
     }
+    else if(ev.keyCode == 32 ){
+        ev.preventDefault();
+        board.playing = !board.playing;
+    }
 });
+board_view.draw();
 
 //  window.addEventListener("load",main);
 window.requestAnimationFrame(controller)
