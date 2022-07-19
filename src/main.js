1ee1127daa757210058b598f5b35701a3bc687ea
JorgeMontoya <Jamontoyao@uqvirtual.edu.co>
2022-07-18 21:00:47 -0500

    se crea el metodo que permite mover las barras

--- src/main.js
+++ src/main.js
@@ -27,17 +27,17 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
-        //this.speed = 10;
+        this.speed = 10;
     }
     self.Bar.prototype = {
         down:function(){
-            //this.y += this.speed;
+            this.y += this.speed;
         },       // para dibujar los elementos
         up: function(){
-            //this.y -= this.speed;
+            this.y -= this.speed;
         },
         toString: function(){
-            //return "x: " + this.x + " y: "+ this.y;
+            return "x: " + this.x + " y: "+ this.y;
         }
     }
 
@@ -53,37 +53,64 @@
         this.ctx = canvas.getContext("2d");
     }
     self.BoardView.prototype={
-        /*clean: function() {
-            this.cxt.clearRect(x=0, y= 0, this.board.width, this.board.height);
-        },*/
+        clean: function() {
+            this.ctx.clearRect(0,  0, this.board.width, this.board.height);
+        },
         draw: function(){
             for(var i = this.board.elements.length - 1; i >= 0 ; i--){
                 var el = this.board.elements[i];
                 draw(this.ctx,el);
             };
+        },
+        play: function(){
+            this.clean();
+
+            this.draw();
         }
     }
     function draw(ctx,element){
         debugger;
-        if(element !== null && !element.hasOwnProperty("Kind")){
+        //if(element !== null && !element.hasOwnProperty("Kind")){
             switch(element.kind){
                 case "rectangle":
                     ctx.fillRect(element.x,element.y,element.width,element.height);
                 break;
             }
-        }
+        //}
     }
 })();
 
-window.addEventListener("load",main);
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar_2 = new Bar(735,100,40,100,board);
 
-function main (){
+var canvas = document.getElementById('canvas');
+var  board_view = new BoardView(canvas,board);
+
+document.addEventListener("keydown", function(ev){
+    ev.preventDefault();
+    console.log(ev.keyCode);
+    if(ev.keyCode == 38){
+        bar.up();
+    }
+    else if(ev.keyCode == 40 ){
+        bar.down();
+    }//w
+    else if(ev.keyCode == 87 ){
+        bar_2.up();
+    }//s
+    else if(ev.keyCode == 83 ){
+        bar_2.down();
+    }
+});
+
+//  window.addEventListener("load",main);
+window.requestAnimationFrame(controller)
+
+function controller (){
     debugger;
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100,board);
+    board_view.play();
+    window.requestAnimationFrame(controller)
 
-    var canvas = document.getElementById('canvas');
-    var  board_view = new BoardView(canvas,board);
-
-    board_view.draw();
+    
 }
