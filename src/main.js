4940d3768e0be7cf94240e659554f95a14deb1ef
JorgeMontoya <Jamontoyao@uqvirtual.edu.co>
2022-07-18 21:39:33 -0500

    se crea elemento del ciculo con sus respectivas caracteristicas

--- src/main.js
+++ src/main.js
@@ -12,11 +12,25 @@
 
         get elements(){
             
-            var elements = this.bars;
-            //elements.Push(this.ball);
+            var elements = this.bars.map(function(bar){return bar;});
+            elements.push(this.ball);
             return elements;
         }
     }
+})();
+(function(){
+     self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        board.ball = this;
+        this.kind = "circle";
+
+
+     }
 })();
 (function(){
     self.Bar = function(x,y,width,height,board){
@@ -75,7 +89,14 @@
                 case "rectangle":
                     ctx.fillRect(element.x,element.y,element.width,element.height);
                 break;
+                case "circle":
+                    ctx.beginPath();
+                    ctx.arc(element.x,element.y,element.radius,0,7);
+                    ctx.fill();
+                    ctx.closePath();
+                break;    
             }
+
         //}
     }
 })();
@@ -86,6 +107,7 @@ var bar_2 = new Bar(735,100,40,100,board);
 
 var canvas = document.getElementById('canvas');
 var  board_view = new BoardView(canvas,board);
+var ball = new Ball(350,100,10, board);
 
 document.addEventListener("keydown", function(ev){
     ev.preventDefault();
